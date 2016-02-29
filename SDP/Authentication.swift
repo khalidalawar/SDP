af80a9659048014c4f11a5496e3713d86fe05de9
Khalid Al awar <khaloodi196@gmail.com>
2016-02-29 19:36:45 +0400

    segregation

--- SDP/Authentication.swift
+++ SDP/Authentication.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+
+class Authentication {
+
+    func create_user_function (let uname : String,let fname : String,let lname : String,let email : String, let pass : String){
+        print("beginning");
+        let request_type = "create_user";
+        
+        
+        
+        let url = NSURL(string: "http://SDP.nfshost.com/authentication.php")
+        let request = NSMutableURLRequest(URL: url!)
+        
+        // modify the request as necessary, if necessary
+        let bodyData = "request_type=" + request_type+"&uname=" + uname + "&fname=" + fname + "&lname=" + lname + "&password=" + pass;
+        
+        request.HTTPMethod = "POST"
+        request.HTTPBody = bodyData.dataUsingEncoding(NSUTF8StringEncoding);
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue())
+            {
+                (response, data, error) in
+                print(NSString(data: data!, encoding: NSUTF8StringEncoding))
+                
+                
+        }
+
+    }
+
+}
--- SDP/ViewController.swift
+++ SDP/ViewController.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var authentication = Authentication();
 
     @IBOutlet weak var password_textfield: UITextField!
     @IBOutlet weak var username_textfield: UITextField!
+    @IBOutlet weak var fname_textfield: UITextField!
+    @IBOutlet weak var lname_textfield: UITextField!
+    @IBOutlet weak var email_textfield: UITextField!
+    
     
     
     override func viewDidLoad() {
@@ -28,6 +34,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func create_user(sender: AnyObject) {
+        
+        authentication.create_user_function(username_textfield.text!,fname: fname_textfield.text!, lname: lname_textfield.text!,email: email_textfield.text!,pass: password_textfield.text!);
 
+    }
 }
 
